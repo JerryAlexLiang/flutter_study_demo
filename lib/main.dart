cd2c9d40aabf308871cc11fd064ba4b1fc12b9eb
JerryAlexLiang <1935990239@qq.com>
2020-01-16 19:05:41 +0800

    [feature] ListView控件2

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,19 @@ class MyApp extends StatelessWidget {
     final wordPair = new WordPair.random();
 
     return new MaterialApp(
-      title: 'Text Widget',
+      title: 'Widget',
       home: new Scaffold(
         //Scaffold 是 Material library 中提供的一个widget,
         // 它提供了默认的导航栏、标题和包含主屏幕widget树的body属性。widget树可以很复杂。
         appBar: new AppBar(
           title: new Text("Text Widget"),
+        ),
+        body: new Center(
+          child: new Container(
+//            height: 200,
+//            child: HorizontalListView(),
+            child: VerticalListView(),
+          ),
         ),
 //        body: new Center(
 //          child: new Container(
@@ -57,87 +64,94 @@ class MyApp extends StatelessWidget {
 ////                fontSize: 25, color: Color.fromARGB(255, 30, 30, 255)),
 ////          ),
 //        ),
-
-        //body: new ListView(
-        //  children:<Widget>[
-        //    new ListTile(
-        //      leading:new Icon(Icons.access_time),
-        //      title:new Text('access_time')
-        //    )
-        //  ]
-        //),
-
-//        body: new ListView(
-//          children: <Widget>[
-//            new ListTile(
-//              leading: new Icon(Icons.access_time),
-//              title: new Text("access_time"),
-//            ),
-//            new ListTile(
-//                leading: new Icon(Icons.account_balance),
-//                title: new Text('account_balance')),
-//            new ListTile(
-//              leading: new Icon(Icons.hot_tub),
-//              title: new Text("hot_tub"),
-//            ),
-//            new Image.network(
-//                "http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
-//            new ListTile(
-//              leading: new Icon(Icons.access_time),
-//              title: new Text("access_time"),
-//            ),
-//            new ListTile(
-//                leading: new Icon(Icons.account_balance),
-//                title: new Text('account_balance')),
-//            new ListTile(
-//              leading: new Icon(Icons.hot_tub),
-//              title: new Text("hot_tub"),
-//            ),
-//            new Image.network(
-//                "http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
-//
-//          ],
-//        ),
-
-//        body: Center(
-//          child: Container(
-//              height: 200.0,
-//              child: new ListView(
-//                scrollDirection: Axis.horizontal,
-//                children: <Widget>[
-//                  new Image.network(
-//                      "http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
-//                  new Container(
-//                    width: 180.0,
-//                    color: Colors.lightBlue,
-//                  ),
-//                  new Image.network(
-//                      "http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
-//                  new Container(
-//                    width: 180.0,
-//                    color: Colors.amber,
-//                  ),
-//                  new Image.network(
-//                      "http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
-//                  new Container(
-//                    width: 180.0,
-//                    color: Colors.deepOrange,
-//                  ),
-//                  new Image.network(
-//                      "http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
-//                  new Container(
-//                    width: 180.0,
-//                    color: Colors.deepPurpleAccent,
-//                  ),
-//                ],
-//              )),
-//        ),
       ),
     );
   }
 }
 
-class MyList extends StatelessWidget {
+class VerticalListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+      children: <Widget>[
+        new ListTile(
+          leading: new Icon(Icons.access_time),
+          title: new Text("access_time"),
+        ),
+        new ListTile(
+            leading: new Icon(Icons.account_balance),
+            title: new Text('account_balance')),
+        new ListTile(
+          leading: new Icon(Icons.hot_tub),
+          title: new Text("hot_tub"),
+        ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
+        new ListTile(
+          leading: new Icon(Icons.access_time),
+          title: new Text("access_time"),
+        ),
+        new ListTile(
+            leading: new Icon(Icons.account_balance),
+            title: new Text('account_balance')),
+        new ListTile(
+          leading: new Icon(Icons.hot_tub),
+          title: new Text("hot_tub"),
+        ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
+        new ListTile(
+          leading: new Icon(Icons.access_time),
+          title: new Text("access_time"),
+        ),
+        new ListTile(
+            leading: new Icon(Icons.account_balance),
+            title: new Text('account_balance')),
+        new ListTile(
+          leading: new Icon(Icons.hot_tub),
+          title: new Text("hot_tub"),
+        ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
+        new ListTile(
+          leading: new Icon(Icons.access_time),
+          title: new Text("access_time"),
+        ),
+        new ListTile(
+            leading: new Icon(Icons.account_balance),
+            title: new Text('account_balance')),
+        new ListTile(
+          leading: new Icon(Icons.hot_tub),
+          title: new Text("hot_tub"),
+        ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
+        new ListTile(
+          leading: new Icon(Icons.access_time),
+          title: new Text("access_time"),
+        ),
+        new ListTile(
+            leading: new Icon(Icons.account_balance),
+            title: new Text('account_balance')),
+        new ListTile(
+          leading: new Icon(Icons.hot_tub),
+          title: new Text("hot_tub"),
+        ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
+        new ListTile(
+          leading: new Icon(Icons.access_time),
+          title: new Text("access_time"),
+        ),
+        new ListTile(
+            leading: new Icon(Icons.account_balance),
+            title: new Text('account_balance')),
+        new ListTile(
+          leading: new Icon(Icons.hot_tub),
+          title: new Text("hot_tub"),
+        ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
+      ],
+    );
+  }
+}
+
+class HorizontalListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -147,18 +161,22 @@ class MyList extends StatelessWidget {
           width: 180,
           color: Colors.red,
         ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
         new Container(
           width: 180,
           color: Colors.yellow,
         ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
         new Container(
           width: 180,
           color: Colors.blue,
         ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
         new Container(
           width: 180,
           color: Colors.greenAccent,
         ),
+        new Image.network("http://dpic.tiankong.com/8g/6d/QJ6316822497.jpg"),
         new Container(
           width: 180,
           color: Colors.orange,
