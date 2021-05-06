3630538c9365218b7a6e3b36b84b11e1ce8e5011
yangliang <yangliang@fosun.com>
2021-05-06 18:05:50 +0800

    [develop] HttpClient请求网络数据

--- lib/net_work_request_page.dart
+++ lib/net_work_request_page.dart
@@ -59,19 +59,5 @@ requestHttpClient() {
     });
   }
 
-  //                     //打开Http连接
-  //                     HttpClientRequest request = await httpClient.getUrl(
-  //                         Uri.parse("https://www.baidu.com"));
-  //                     //使用iPhone的UA
-  //                     request.headers.add("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
-  //                     //等待连接服务器（会将请求信息发送给服务器）
-  //                     HttpClientResponse response = await request.close();
-  //                     //读取响应内容
-  //                     _text = await response.transform(utf8.decoder).join();
-  //                     //输出响应头
-  //                     print(response.headers);
-  //
-  //                     //关闭client后，通过该cli
-
   getHttpClient();
 }
